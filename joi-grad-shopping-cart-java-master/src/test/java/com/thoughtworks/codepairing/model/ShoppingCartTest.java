33d885b803b00db766bd19874c4608409d66bbba
cylwmm <382366956@qq.com>
2021-10-14 21:23:49 +0800

    TW面试

--- joi-grad-shopping-cart-java-master/src/test/java/com/thoughtworks/codepairing/model/ShoppingCartTest.java
+++ joi-grad-shopping-cart-java-master/src/test/java/com/thoughtworks/codepairing/model/ShoppingCartTest.java
@@ -19,6 +19,7 @@ public class ShoppingCartTest {
     public void setUp() throws Exception {
         customer = new Customer("test");
     }
+
     @Test
     public void shouldCalculatePriceWithNoDiscount() {
         List<Product> products = asList(new Product(PRICE, "", PRODUCT));
